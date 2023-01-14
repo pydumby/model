b6f0f722e0e7177150bd6417ccbaa6e95f2cb748
PyDumby <bizzbusy321@gmail.com>
2023-01-14 23:19:07 +0100

    first commit

--- model.py
+++ model.py
@@ -1,9 +1,24 @@
 import __future__
 from builtins import print
 
+import random
+
 class ai:
+    
+    comment = []
+    
     def __init__(self) -> None:
         pass
 
-    def reply():
-        print(comment)
+    def length(c):
+        print("length:", len(c))
+
+    def greet():
+        greeting = ["hello.", "hi!", "how are you?", "how are you doing?"]
+        rnumber = int(random.randrange(len(greeting)))
+        print(greeting[rnumber])
+
+
+
+
+ai.greet()
